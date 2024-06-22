0cac44b9f09ed44d9b0e2fb155107c1d7aa3303b
RMHamzaArif89 <ranaarifisonline@gmail.com>
2024-06-22 08:48:29 +0500

    created the signup schma for user

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -160,6 +160,103 @@ router.patch('/updateProduct/:id',async(req,res)=>{
 
 
 
+
+router.post('/signup',async(req,res)=>{
+ 
+  try{
+    const emailExist=await UserDetail.findOne({email:req.body.email})
+    if(emailExist){
+      return  res.status(400).json('email already exist')
+    }
+      const userData=new UserDetail({
+          name:req.body.name,
+          age:req.body.age,
+          email:req.body.email,
+          password:req.body.password
+      })
+      //or simply second method
+     // const userData=new userSchema(req.body)
+     const token= await userData.generateToken()
+     const refreshToken= await userData.generateRefreshToken()
+
+     const create= await UserDetail.create(userData)
+     const options= {
+      httpOnly:true,
+      secure:true,
+      maxAge:100000,
+      sameSite:'strict'
+     }
+   if(create){
+    return   res.status(200)
+    .cookie("accessToken", token,options)
+    .cookie("refreshToken",refreshToken,options)
+    .json(userData)
+   }
+      
+  }
+ 
+  catch(err){
+  return  res.status(400).json('false')
+  }
+})
+
+
+router.post('/login', async(req,res)=>{
+// console.log('cookietoken',req.cookies.accessToken)
+
+try{
+  const email=req.body.email;
+  const password=req.body.password;
+  console.log('login',password)
+  if(!email ||  !password){
+    return  res.status(400).json('please fill the form feilds')
+  }
+  const data= await UserDetail.findOne({email:email})
+  if(!data){
+    console.log('not find data')
+    return  res.status(400).json('invalid login details')
+  }
+
+ 
+  const passwordMatch= await bcrypt.compare(password,data.password)
+  const token=await data.generateToken()
+  const refreshToken= await data.generateRefreshToken()
+  const options= {
+    httpOnly:true,
+    secure:true,
+    maxAge:300000,
+    sameSite:'strict'
+   }
+   if(!passwordMatch){
+    console.log('not match pwd')
+   }
+
+ 
+
+  if(passwordMatch){
+      console.log('password match')
+      return res.status(200)
+      .cookie("accessToken", token, options)
+      .cookie("refreshToken",refreshToken,options)
+      .json({
+        id:data._id
+      })
+// console.log('okay')
+      
+  }else{
+    return  res.status(400).json('invalid login details')
+      console.log('not okay')
+  }
+}
+catch(e){
+  res.status(400).send(e)
+}
+  
+})
+
+
+
+
  
 
 module.exports=router;
